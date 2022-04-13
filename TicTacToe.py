483346f297d562342c99ad5cd1e36aef73c197ec
UnkNownCoDa <94311737+UnkNownCoDa@users.noreply.github.com>
2022-04-13 14:46:23 +0530

    Update TicTacToe.py

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,17 +1,18 @@
+#Creating the board
 board = [[" "," "," "],[" "," "," "],[" "," "," "]]
-count = 0
+count = 0 #Keeps track of how full the board is
 def play(player, row, col):
-    board[row][col] = player
-    for i in board:
+    board[row][col] = player #Places the selection of the player
+    for i in board: #Displays the board on the screen
         print(i)
-    if board[0][0] == board[1][1] == board[2][2] == player:
+    if board[0][0] == board[1][1] == board[2][2] == player: #checks top left diagonal 
         return player
-    if board[2][0] == board[1][1] == board[0][2] == player:
+    if board[2][0] == board[1][1] == board[0][2] == player: #checks top right diagonal
         return player
     for i in range(0, 3):
-        if board[i][0] == board[i][1] == board[i][2] == player:
+        if board[i][0] == board[i][1] == board[i][2] == player: #checks the rows
             return board[i][1]
-        if board[0][i] == board[1][i] == board[2][i] == player:
+        if board[0][i] == board[1][i] == board[2][i] == player: #checks the columns
             return board[0][i]
     return "none" 
 player = 'X'
